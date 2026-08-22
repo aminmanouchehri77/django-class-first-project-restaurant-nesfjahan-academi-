79b1ebd02ff9ff7462fa4659c770e1d72d95d5cf
amin manouchehri <aminmanouchehri@gmail.com>
2026-08-22 14:10:06 +0430

    form in django (first_section)

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category,Foods
+from .models import Category,Foods,Review
 
 admin.site.register(Category)
 admin.site.register(Foods)
+admin.site.register(Review)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,8 @@
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from .models import Foods,Category
-
+from .models import Foods,Category,Review
+from .forms import ReviewForm
 
 def menu(request):
     categories=Category.objects.all()
@@ -13,4 +13,16 @@ def menu(request):
 
 def food_detail_view(request,pk):
     food=get_object_or_404(Foods,pk=pk)
-    return render(request,"restaurant/food_detail.html",context={'food':food})
+    reviews=food.reviews.all()
+    if request.method=='POST':
+        comment=ReviewForm(request.POST)
+        if comment.is_valid():
+            new_comment=comment.save(commit=False)
+            new_comment.food=food
+            new_comment.status=Review.STATUS_CHOICES[0]
+            new_comment.save()
+            comment=ReviewForm()
+    else:
+        comment=ReviewForm()
+        
+    return render(request,"restaurant/food_detail.html",context={'food':food,'form':comment,'reviews':reviews})
